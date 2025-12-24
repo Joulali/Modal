9e644c91fb9fc4441189bf3a458ba78dbd762e6e
joulali_mahdi <joulali.mehdi@gmail.com>
2025-12-24 17:02:19 +0100

    set types of modal right, left and center

--- src/app/modal/modal.animations.ts
+++ src/app/modal/modal.animations.ts
@@ -1,21 +1,28 @@
 import { trigger, state, style, animate, transition } from '@angular/animations';
 
 export const MODAL_ANIMATION = trigger('modalAnimation', [
-  state('show', style({
-    opacity: 1,
-    transform: 'translate(-50%, -50%) scale(1)',
-  })),
-  state('hide', style({
-    opacity: 0,
-    transform: 'translate(-50%, -50%) scale(0.9)',
-  })),
-  transition('hide => show', animate('0.25s ease-in')),
-  transition('show => hide', animate('0.3s ease-out')),
+  state(
+    'hide',
+    style({
+      opacity: 0,
+      transform: '{{ hideTransform }}',
+    }),
+    { params: { hideTransform: 'translate(-50%, -50%) scale(0.9)' } }
+  ),
+  state(
+    'show',
+    style({
+      opacity: 1,
+      transform: '{{ showTransform }}',
+    }),
+    { params: { showTransform: 'translate(-50%, -50%) scale(1)' } }
+  ),
+  transition('hide => show', animate('250ms ease-out')),
+  transition('show => hide', animate('200ms ease-in')),
 ]);
 
 export const BACKDROP_ANIMATION = trigger('backdropAnimation', [
-  state('show', style({ opacity: 1 })),
   state('hide', style({ opacity: 0 })),
-  transition('hide => show', animate('0.25s ease-in')),
-  transition('show => hide', animate('0.25s ease-out')),
+  state('show', style({ opacity: 1 })),
+  transition('hide <=> show', animate('200ms')),
 ]);
